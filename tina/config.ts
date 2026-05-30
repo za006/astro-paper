01afeec9c3d3207540008f36d9f7a1cec30903ca
za006 <134183191+za006@users.noreply.github.com>
2026-05-30 17:56:01 +0800

    Create config.ts

---
+++ tina/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tinacms";
+export default defineConfig({
+  branch: "main",
+  clientId: "3a42b7a4-ed14-4b23-b3db-6f2da3503d32",
+  token: "8a8c223dc6a326e71053abb3d5d0f0e3bdb0439f",
+  build: { outputFolder: "admin", publicFolder: "public" },
+  schema: { collections: [] },
+});
